84722d235646b3941b401ec86f465553f5dcc3a2
davisanderson11 <davis@rsmla.com>
2025-06-02 11:19:25 -0400

    Initial commit

---
+++ bin/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const inquirer = require('inquirer');
+const createApp = require('../lib/create-app');
+
+program
+  .version('1.0.0')
+  .argument('[project-name]', 'Name of the project')
+  .option('-t, --template <template>', 'Template to use', 'default')
+  .action(async (projectName, options) => {
+    if (!projectName) {
+      const answers = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'projectName',
+          message: 'What is your project name?',
+          default: 'my-jspsych-app'
+        }
+      ]);
+      projectName = answers.projectName;
+    }
+
+    await createApp(projectName, options);
+  });
+
+program.parse();
